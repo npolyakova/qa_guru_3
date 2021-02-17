95822f0d7cd1b071f83a3addb70202c7a3cc6b29
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-02-17 08:35:23 +0300

    fixes

--- src/test/java/GithubTesting.java
+++ src/test/java/GithubTesting.java
@@ -2,11 +2,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
-public class githubTesting {
+public class GithubTesting {
 
     @Test
     void githubSelenideTesting() {
@@ -14,13 +15,12 @@ public class githubTesting {
         open("selenide/selenide");
 
         $("[data-hotkey='g w']").click();
-        assert (url().equals(Configuration.baseUrl + "selenide/selenide/wiki"));
+        $("#user-content-welcome-to-the-selenide-wiki").shouldBe(visible);
 
         $(".markdown-body").shouldHave(text("Soft assertions"));
         $(byText("SoftAssertions")).click();
         assert (url().equals(Configuration.baseUrl + "selenide/selenide/wiki/SoftAssertions"));
 
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
-        sleep(2000);
     }
 }
